296281a8a5c03392fdbb3e1665e52855066226c8
kimjiwon <jwkim.oa@gmail.com>
2021-06-26 18:40:43 +0900

    Solve: 1-2.다음큰숫자

---
+++ 1-2.다음큰숫자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int nCountOf1s = countOf1s(n);
+        for (int i = n + 1; i <= 1_000_000; i++) {
+            if (nCountOf1s == countOf1s(i))
+                return i;
+        }
+        return -1;
+    }
+
+    private int countOf1s(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 2;
+            x >>= 1;
+        }
+        return sum;
+    }
+}
