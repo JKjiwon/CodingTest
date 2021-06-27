a4a8b0c85af5c61ed3bebdcfe0505d8e447c4403
kimjiwon <jwkim.oa@gmail.com>
2021-06-27 14:26:31 +0900

    Improve: 1-6.소수만들기 - 재귀 종료 조건을 조금 더 개선해보자

--- prgrms/1-6.소수만들기.java
+++ prgrms/1-6.소수만들기.java
@@ -2,6 +2,9 @@
  * 주어진 숫자 중 3개의 수를 더해 소수가 되는 경우의 수
  * 클래스 필드 변수 사용 안하기
  * 어떻게 뽑냐? 조합!!
+ * 
+ * 조금만 더 개선해보자.
+ * 뽑아야하는 개수보다 뽑을수 있는 수가 적으면 더이상 작업을 진행하는것은 의미가 없다.
  */
 class Solution {
     public int solution(int[] nums) {
@@ -11,18 +14,18 @@ class Solution {
     private int solve(int index, int selectCount,int sum, int[] nums) {
 
         if (selectCount == 3) {
-            if (isPrime(sum)) {
+            if (isPrime(sum))
                 return 1;
-            }
             return 0;
         }
-
-        if (index == nums.length) {
+        // 뽑을 수 있는 개수 < 더 뽑아야 하는 수
+        if ((nums.length - index) < (3 - selectCount))
             return 0;
-        }
+        
         int countOfPrime = 0;
         countOfPrime += solve(index + 1, selectCount + 1, sum + nums[index], nums);
         countOfPrime += solve(index + 1, selectCount, sum, nums);
+
         return countOfPrime;
     }
 
