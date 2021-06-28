badbc9858e43e97fc82b06baca69ef22b5b00a49
kimjiwon <jwkim.oa@gmail.com>
2021-06-28 12:10:40 +0900

    Solve: 2-3. 주식 가격

--- prgrms/chapter02/2-3.주식 가격.java
+++ prgrms/chapter02/2-3.주식 가격.java
@@ -1,6 +1,28 @@
+import java.util.Stack;
+
+/**
+ * stack : 아직까지 떨어지지 않은 인덱스의 모음
+ */
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
-        return answer;
+
+        Stack<Integer> st = new Stack<>();
+        int[] answers = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!st.empty() && prices[i] < prices[st.peek()]) { // i의 수가 이전 보다 감소하는 때.
+                answers[st.peek()] = i - st.peek();
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while (!st.empty()) {
+            answers[st.peek()] = prices.length - 1 - st.peek();
+            st.pop();
+        }
+
+        return answers;
     }
 }
