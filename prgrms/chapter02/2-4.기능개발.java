2b591a570d7509063ae35bed0866d0c6e486faa2
kimjiwon <jwkim.oa@gmail.com>
2021-06-28 14:22:46 +0900

    Improve: 2-4. 기능 개발 - Queue를 이용한 풀이

--- prgrms/chapter02/2-4.기능개발.java
+++ prgrms/chapter02/2-4.기능개발.java
@@ -1,28 +1,37 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+/**
+ * 일이 몇일 남았나? 를 계산해야 한다.
+ * Queue : Queue<Integer> leftDate = new LinkedList<>();
+ * offer() , poll() , isEmpty()
+ * 
+ * 중요 : 정수 / 정수 = 정수가 아닌 유리수 => 정수 / (double) 정수 => 즉 캐스팅이 필요하다.
+ * ex) Math.ceil(70/30) = 2.0
+ *     Math.ceil(70/(float)30) = 3.0
+ */
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        int finishedIndex = 0;
-        boolean[] finished = new boolean[progresses.length];
-        while (true) {
-            for (int i = 0; i < progresses.length; i++) {
-                progresses[i] += speeds[i];
-            }
+        Queue<Integer> leftDate = new LinkedList<>();
 
-            int count = 0;
-            while (finishedIndex < progresses.length && progresses[finishedIndex] >= 100) {
-                finished[finishedIndex] = true;
+        for (int i = 0; i < progresses.length; i++) {
+            int leftProgess = 100 - progresses[i];
+            int date = (int) Math.ceil(leftProgess / (double) speeds[i]);
+            leftDate.offer(date);
+        }
+
+        while (!leftDate.isEmpty()) {
+            int left = leftDate.poll();
+            int count = 1;
+            while (!leftDate.isEmpty() && left >= leftDate.peek()) {
                 count++;
-                finishedIndex++;
+                leftDate.poll();
             }
-            if (count != 0) {
-                answer.add(count);
-            }
-
-            if (finished[progresses.length - 1])
-                break;
+            answer.add(count);
         }
         
         return answer.stream().mapToInt(i -> i).toArray();
