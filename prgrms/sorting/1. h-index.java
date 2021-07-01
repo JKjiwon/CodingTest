50a0688a56616a821085e595205cec37554c2eaf
kimjiwon <jwkim.oa@gmail.com>
2021-07-02 00:06:07 +0900

    Improve: h-index - small변수 제거

--- prgrms/sorting/1. h-index.java
+++ prgrms/sorting/1. h-index.java
@@ -6,21 +6,15 @@ class Solution {
         Arrays.sort(citations);
 
         int answer = 0;
-        for (int h = 0; h < len + 1; h++) {
-            // 인용 횟수가 h번 보다 크거나 같은 수
+        for (int h = 0; h < len+1; h++) {            
+            // i 보다 크거나 같은 수
             int big = 0;
-            // 인용 횟수가 h번 보다 작은 수
-            int small = 0;
-
-            for (int j = 0; j < len; j++) {
+            for(int j = 0; j < len; j++) {
                 if (citations[j] >= h) {
                     big++;
-                } else {
-                    small++;
                 }
             }
-
-            if (big >= h && small <= h) {
+            if(big >= h) {
                 answer = h;
             }
         }
